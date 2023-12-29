b6bdf50126bd5dc9096050fe014f890c4b63805c
Altenir Modesto Gomes <PandinhaAnao12@proton.me>
2023-12-29 17:10:50 -0400

    Terminando o valida senha, falta verificar os epecias

--- src/midleware/ValidaSenha.js
+++ src/midleware/ValidaSenha.js
@@ -2,7 +2,32 @@ const validaSenha =
     (req,res,next)=>{
         const senha = req.body.Sehna;
         if(senha){
-            if(senha.length>=8){
+            if(senha.length>=8){                
+                const minusculos = 'abcdefghijklmnopqrstuvwxyz';
+                const maiuscula = minusculos.toUpperCase();
+                const numeros = '123456789';
+                const especiais = maiuscula.concat(maiuscula).concat(numeros);
+                function varificaDados(metaDado,stringSenha){
+                    let fazer = true;
+                    let temOcaracter = false;
+                    let tamanhoMetaDado = metaDado.length;
+                    let tamanhoSenha = stringSenha.length;
+                    for(let i=0;i<tamanhoMetaDado;i++){
+                        if(fazer){
+                            for(let c=0;c<tamanhoSenha;c++){
+                                if(metaDado[i]===stringSenha[c]){
+                                    temOcaracter = true;
+                                    fazer = false;
+                                    break;
+                                };
+                            };
+                        }else{
+                            break;
+                        }
+                    };
+                    return temOcaracter;
+                };
+                let aprovado = (varificaDados(minusculos,senha)&&varificaDados(maiuscula,senha)&&varificaDados(numeros,senha));
             }else{
                 res.status(400).json({
                     Mensage:'A senha que voce inseriu é invalida pois nao possui o tamanho adequado',
@@ -25,3 +50,5 @@ export default validaSenha;
 //Minuscula
 //Numeros
 //Espesciais
+
+//sperar a senha
